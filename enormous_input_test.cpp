fca4ab2f0e2e3676716bfcc07dfcce89728e773c
Tushar <codingid6@gmail.com>
2020-08-06 19:58:08 +0530

    more code

---
+++ enormous_input_test.cpp
@@ -0,0 +1,23 @@
+//
+#include<bits/stdc++.h>
+
+#define mod 1000000007
+#define lli long long int
+#define endl '\n'
+using namespace std;
+#define REP(i,n) for (int i = 0; i < n; i++)
+
+ 
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
